81f2ff487bd1906eee643d1dc95fabb37538a8db
Roma Kolesnyk <romakoles1@ukr.net>
2024-03-22 23:52:52 +0200

    edge data logic

--- smart_car_edge_data_logic/app/adapters/agent_mqtt_adapter.py
+++ smart_car_edge_data_logic/app/adapters/agent_mqtt_adapter.py
@@ -24,11 +24,16 @@ class AgentMQTTAdapter(AgentGateway):
 
     def on_message(self, client, userdata, msg):
         """Processing agent data and sent it to hub gateway"""
+        logging.info(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
         try:
-            payload: str = msg.payload.decode("utf-8")
+            payload: str = msg.payload.decode()
             agent_data = AgentData.model_validate_json(payload, strict=True)
             processed_data = process_agent_data(agent_data)
-            if not self.hub_gateway.save_data(processed_data):
+
+            json_processed_agent_data = processed_data.json()
+            json_processed_agent_data_batch = [json_processed_agent_data]
+
+            if not self.hub_gateway.save_data(processed_agent_data_batch=json_processed_agent_data_batch):
                 logging.error("Hub is not available")
         except Exception as e:
             logging.info(f"Error processing MQTT message: {e}")
@@ -36,7 +41,7 @@ class AgentMQTTAdapter(AgentGateway):
     def connect(self):
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-        self.client.connect(self.broker_host, self.broker_port, 60)
+        self.client.connect(self.broker_host, self.broker_port)
 
     def start(self):
         self.client.loop_start()
--- smart_car_edge_data_logic/app/usecases/data_processing.py
+++ smart_car_edge_data_logic/app/usecases/data_processing.py
@@ -2,7 +2,7 @@ from app.entities.agent_data import AgentData
 from app.entities.processed_agent_data import ProcessedAgentData
 
 
-def process_agent_data(agent_data: AgentData,) -> ProcessedAgentData:
+def process_agent_data(agent_data: AgentData) -> ProcessedAgentData:
     """
     Process agent data and classify the state of the road surface.
     Parameters:
@@ -11,4 +11,5 @@ def process_agent_data(agent_data: AgentData,) -> ProcessedAgentData:
     processed_data (ProcessedAgentData): Processed data containing the classified state of
     the road surface and agent data.
     """
-    pass
+    return ProcessedAgentData(road_state="stable" if agent_data.accelerometer.z < 50 else "critic",
+                              agent_data=agent_data)
